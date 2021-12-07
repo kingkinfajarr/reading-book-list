efbedf579f93b3995c8ea8795616f1bf84e23e86
kingkinfajarr <kingkinfajar8@gmail.com>
2021-12-07 13:30:11 +0700

    fix some styling

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,7 +4,7 @@ import ToggleTheme from './ToggleTheme';
 const Header = () => {
   return (
     <header className="flex justify-between items-center pt-14 mb-10">
-      <h1 className="md:text-5xl text-3xl font-bold text-green-500 ">
+      <h1 className="md:text-5xl text-2xl font-bold text-green-500 ">
         Reading Book List
       </h1>
       <ToggleTheme />
